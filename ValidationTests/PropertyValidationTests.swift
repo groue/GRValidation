69ad0b9c179920a779e466921f3142bc2bbb307b
Gwendal Roué <gr@pierlis.com>
2015-08-21 06:50:22 +0200

    WIP

--- ValidationTests/PropertyValidationTests.swift
+++ ValidationTests/PropertyValidationTests.swift
@@ -17,35 +17,32 @@ struct Model {
     let value1: Int?
     let value2: Int?
     
-    func validate() throws {
-        
-        // Name must not be empty
-        let nameValidation = ValidationStringNotEmpty()
-        
-        // Age >= 0
-        let ageValidation = ValidationRange(minimum: 0)
-        
-        // Card number must be nil, or, if present, contain at least 10 characters.
-        // TODO: the error description contains "cardNumber should be nil. cardNumber should contain at least 10 characters." which is confusing.
-        let cardNumberValidation = ValidationNil<String>() || ValidationStringLength(minimum: 10)
-        
-        // MagicWord must contain "foo" and "bar".
-        let magicWordValidation = ValidationRegularExpression(pattern: "foo") && ValidationRegularExpression(pattern: "bar")
-        
+    // Name must not be empty
+    static let nameValidation = ValidationStringNotEmpty()
+    
+    // Age >= 0
+    static let ageValidation = ValidationRange(minimum: 0)
+    
+    // Card number must be nil, or, if present, contain at least 10 characters.
+    // TODO: the error description contains "cardNumber should be nil. cardNumber should contain at least 10 characters." which is confusing.
+    static let cardNumberValidation = ValidationNil<String>() || ValidationStringLength(minimum: 10)
+    
+    // MagicWord must contain "foo" and "bar".
+    static let magicWordValidation = ValidationRegularExpression(pattern: "foo") && ValidationRegularExpression(pattern: "bar")
+    
+    static let validation = Validation<Model>() >>> (
+        PropertyValidation("name", { $0.name }, nameValidation)
+        && PropertyValidation("age", { $0.age }, ageValidation)
+        && PropertyValidation("magicWord", { $0.magicWord }, magicWordValidation)
+        && PropertyValidation("cardNumber", { $0.cardNumber }, cardNumberValidation)
         // TODO: Global validation should be able to be written alone, while keeping the code readable
-        let value1Validation = ValidationNotNil<Int>()
-        let value2Validation = ValidationNotNil<Int>()
-        
-        let v = Validation<Model>() >>> (
-                PropertyValidation("name", { $0.name }, nameValidation)
-                && PropertyValidation("age", { $0.age }, ageValidation)
-                && PropertyValidation("magicWord", { $0.magicWord }, magicWordValidation)
-                && PropertyValidation("cardNumber", { $0.cardNumber }, cardNumberValidation)
-                && GlobalValidation("Value1 or Value2 must be not nil.",
-                    PropertyValidation("value1", { $0.value1 }, value1Validation)
-                    || PropertyValidation("value2", { $0.value2 }, value2Validation))
-        )
-        try v.validate(self)
+        && GlobalValidation("Value1 or Value2 must be not nil.",
+            PropertyValidation("value1", { $0.value1 }, ValidationNotNil<Int>())
+            || PropertyValidation("value2", { $0.value2 }, ValidationNotNil<Int>()))
+    )
+    
+    func validate() throws {
+        try Model.validation.validate(self)
     }
 }
 
